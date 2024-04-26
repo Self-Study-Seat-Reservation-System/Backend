c1414f8877808097c3f894686bfd330cb35360e5
Ruoshi Sun <56506650+moyiii-ai@users.noreply.github.com>
2024-04-26 22:22:49 +0800

    [feat] create room

--- app/models/room.py
+++ app/models/room.py
@@ -15,7 +15,7 @@ from db import db
 class Room(db.Model):
     __tablename__ = "room"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     building_id = db.Column(db.Integer, nullable=False)
     school = db.Column(db.String(50))
     name = db.Column(db.String(100), nullable=False)
