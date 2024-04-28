bab2da4971beb2d42439ed17e06440f29174c7ef
Ruoshi Sun <56506650+moyiii-ai@users.noreply.github.com>
2024-04-28 20:38:16 +0800

    [refactor] sort app

--- app/app.py
+++ app/app.py
@@ -3,14 +3,14 @@ from flask_restful import Api
 
 from initializers import mysql
 
+from resources.admin_config import AdminConfigResource
+from resources.administer import AdministerResource
 from resources.building import BuildingResource
 from resources.hello import Hello
+from resources.reservation import ReservationResource
 from resources.room import RoomResource
 from resources.seat import SeatResource
-from resources.reservation import ReservationResource
-from resources.admin_config import AdminConfigResource
 from resources.student import StudentResource
-from resources.administer import AdministerResource
 
 
 app = Flask(__name__)
@@ -18,6 +18,13 @@ app.config.from_object("config.Config")
 
 db = mysql.init_db(app)
 api = Api(app)
+
+
+# adminconfig
+api.add_resource(AdminConfigResource, "/api/adminconfig")
+
+# administer
+api.add_resource(AdministerResource, "/api/administer")
 
 # building
 api.add_resource(BuildingResource, "/api/building", endpoint="building")
@@ -40,10 +47,6 @@ api.add_resource(SeatResource, "/api/seat/<int:seat_id>", endpoint="seat_by_id")
 # student
 api.add_resource(StudentResource, "/api/student")
 
-# adminconfig
-api.add_resource(AdminConfigResource, "/api/adminconfig")
-# administer
-api.add_resource(AdministerResource, "/api/administer")
 
 if __name__ == "__main__":
     app.run(debug=True)
