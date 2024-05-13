556309731e63b47e57f5da1acc7a704656e5c7d3
Ruoshi Sun <56506650+moyiii-ai@users.noreply.github.com>
2024-05-13 16:13:09 +0800

    [test] test create duplicate building

--- app/tests/test_building.py
+++ app/tests/test_building.py
@@ -5,16 +5,16 @@ sys.path.append(os.path.dirname(__file__))
 from base_test import BasicTest, BasicUtil
 
 class BuildingUtil(BasicUtil):
-    def create_building(self):
+    def create_building(self, name="Name1", campus="H", open_time="08:00:00", close_time="17:00:00"):
         headers = {
             "Content-Type": "application/json"
         }
 
         data = {
-            "name": "Building Name",
-            "campus": "Campus Name",
-            "open_time": "08:00:00",
-            "close_time": "17:00:00"
+            "name": name,
+            "campus": campus,
+            "open_time": open_time,
+            "close_time": close_time
         }
 
         response = self.app.post("/api/building", headers=headers, json=data)
@@ -28,3 +28,8 @@ class BuildingTest(BasicTest):
     def test_building_create_successfully(self):
         response = self.building_util.create_building()
         self.assertEqual(response.status_code, 201)
+
+    def test_building_create_duplicate(self):
+        response = self.building_util.create_building("Name1")
+        response = self.building_util.create_building("Name1")
+        self.assertEqual(response.status_code, 400)
