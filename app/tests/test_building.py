c4f80c4222db0551855f3d4c8de0c99bd0e61296
Ruoshi Sun <56506650+moyiii-ai@users.noreply.github.com>
2024-05-13 16:30:41 +0800

    [test] test create building about time

--- app/tests/test_building.py
+++ app/tests/test_building.py
@@ -25,11 +25,23 @@ class BuildingTest(BasicTest):
     def setUp(self):
         self.building_util = BuildingUtil()
     
-    def test_building_create_successfully(self):
+    def test_create_building_successfully(self):
         response = self.building_util.create_building()
         self.assertEqual(response.status_code, 201)
 
-    def test_building_create_duplicate(self):
+    def test_create_synonym_building(self):
         response = self.building_util.create_building("Name1")
         response = self.building_util.create_building("Name1")
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_building_with_wrong_time_slot(self):
+        response = self.building_util.create_building(open_time="19:23:45", close_time="08:00:00")
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_building_with_bad_format_time(self):
+        response = self.building_util.create_building(open_time="08:00:00", close_time="09:0000")
+        self.assertEqual(response.status_code, 400)
+        response = self.building_util.create_building(open_time="08:00:00", close_time="09:00!00")
+        self.assertEqual(response.status_code, 400)
+        response = self.building_util.create_building(open_time="08:00:00", close_time="9:00:00")
         self.assertEqual(response.status_code, 400)
