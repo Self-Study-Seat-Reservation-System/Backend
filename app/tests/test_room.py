8e328347df53d9d3ea112e7cfe55a01c94c43b2c
Ruoshi Sun <56506650+moyiii-ai@users.noreply.github.com>
2024-05-19 21:29:39 +0800

    [test] test update room successfully

--- app/tests/test_room.py
+++ app/tests/test_room.py
@@ -6,7 +6,7 @@ from base_test import BasicTest, BasicUtil
 from test_building import BuildingUtil
 
 class RoomUtil(BasicUtil):
-    def create_room(self, building_id=1, school="", name="A6025", open_time="08:00:00", close_time="17:00:00"):
+    def create_room(self, building_id=1, school=None, name="A6025", open_time="08:00:00", close_time="17:00:00"):
         data = {
             "building_id": building_id,
             "name": name,
@@ -14,10 +14,22 @@ class RoomUtil(BasicUtil):
             "close_time": close_time
         }
 
-        if school:
+        if school is not None:
             data["school"] = school
 
         response = self.app.post("/api/room", headers=self.headers, json=data)
+        return response
+
+    def update_room(self, room_id, open_time=None, close_time=None, deprecated=None):
+        data = {}
+        if open_time is not None:
+            data["open_time"] = open_time
+        if close_time is not None:
+            data["close_time"] = close_time
+        if deprecated is not None:
+            data["deprecated"] = deprecated
+        
+        response = self.app.put(f"/api/room/{room_id}", headers=self.headers, json=data)
         return response
 
     def delete_room(self, room_id):
@@ -53,3 +65,9 @@ class RoomTest(BasicTest):
     def test_delete_room_without_room(self):
         response = self.room_util.delete_room(1)
         self.assertEqual(response.status_code, 404)
+
+    def test_update_room_successfully(self):
+        self.building_util.create_building()
+        self.room_util.create_room()
+        response = self.room_util.update_room(1, "15:00:00", "18:00:00", 0)
+        self.assertEqual(response.status_code, 200)
