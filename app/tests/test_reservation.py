af2be5701a762809601963ec49a71e7b7ed7ba80
Ruoshi Sun <56506650+moyiii-ai@users.noreply.github.com>
2024-05-20 20:19:47 +0800

    [test] test create reservation with deprecated room

--- app/tests/test_reservation.py
+++ app/tests/test_reservation.py
@@ -62,4 +62,11 @@ class ReservationTest(BasicTest):
         self.student_util.create_student()
         response = self.reservation_util.create_reservation(room_id=2)
         self.assertEqual(response.status_code, 404)
-        
+
+    def test_create_reservation_with_deprecated_room(self):
+        self.room_util.create_room()
+        self.seat_util.create_seat()
+        self.room_util.delete_room(1)
+        self.student_util.create_student()
+        response = self.reservation_util.create_reservation()
+        self.assertEqual(response.status_code, 400)
